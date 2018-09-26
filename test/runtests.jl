d13412d473a27e627a1d228964e8f6fc2658cc24
pevnak <pevnak@gmail.com>
2018-09-26 08:12:42 +0200

    some changes in runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,6 @@ end
 		"b" => [6 7], 
 		"c" => reshape([8], (1,1)))
 	loadfun(f) = ArrayNode(d[f])
-	# DataIterators.catobs(x::T...) where {T<:Mill.AbstractNode} = cat(x...)
 
 	@test all([x.data for x in FileIterator(loadfun, ["a", "b", "c"], 2)] .== [[1 2], [3 4], [5 6], [7 8]])
 	@test all([x.data for x in FileIterator(loadfun, ["a", "b"], 2)] .== [[1 2], [3 4], [5 6], reshape([7],1 ,1)])
