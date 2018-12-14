141253741e56642dbc4528bb8f4c76453a3ea2f9
pevnak <pevnak@gmail.com>
2018-12-14 12:53:12 +0100

    Loading function should output nothing rather than empty datasets

--- src/fileiterator.jl
+++ src/fileiterator.jl
@@ -15,7 +15,7 @@ struct FileIterator{A,B}
 end
 
 function Base.iterate(ffl::FileIterator, state = (nothing, 1))
-  x, i = loadnextbatch(ffl.loadfun, ffl.files, ffl.nobs, state...)[1:2]
+  x, i = loadnextbatch(ffl.loadfun, ffl.files, ffl.nobs, state...)
   x == nothing && return(nothing)
   nobs(x) == 0 && return(nothing)
   x, xx = splitdata(x, ffl.nobs)
@@ -31,14 +31,14 @@ end
 """
 function loadnextbatch(loadfun, files, n, x, i)
   while (i <=length(files)) && ( x == nothing || nobs(x) < n)
-    try 
+    # try 
       xx = loadfun(files[i])
-      xx == nothing && return(x, i)
+      # xx == nothing && return(x, i)
       x = filteredcat(x, xx)
-    catch me 
-      @warn "error while loading $(files[i])"
-      println(me)
-    end
+    # catch me 
+      # @warn "error while loading $(files[i])"
+      # println(me)
+    # end
     i += 1
   end 
   x, i
